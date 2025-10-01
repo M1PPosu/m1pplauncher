21ac6c068dbb8164d972c277baa2334045aba0b5
minisbett <39670899+minisbett@users.noreply.github.com>
2025-10-01 23:21:07 +0200

    Scale absolute window scaling with windows DPI

---
+++ m1pplauncher/Utils/WinApi.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.InteropServices;
+using Microsoft.UI.Xaml;
+using WinRT.Interop;
+
+namespace m1pplauncher.Utils;
+
+internal static class WinApi
+{
+  [DllImport("user32.dll")]
+  private static extern uint GetDpiForWindow(nint hWnd);
+
+  public static double GetScreenScaling(Window window) => GetDpiForWindow(WindowNative.GetWindowHandle(window)) / 96;
+}
--- m1pplauncher/Views/MainWindow.xaml.cs
+++ m1pplauncher/Views/MainWindow.xaml.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Runtime.InteropServices;
+using m1pplauncher.Utils;
 using Microsoft.UI.Windowing;
 using Microsoft.UI.Xaml;
 using Windows.Graphics;
@@ -13,12 +16,13 @@ public sealed partial class MainWindow : Window
     AppWindow.SetIcon("Assets/LogoSquare.png");
     Title = "M1PP Lazer Launcher";
 
-    AppWindow.Resize(new(500, 258));
     ExtendsContentIntoTitleBar = true;
     SetTitleBar(MainPage.AppTitleBar);
     
     DisplayArea displayArea = DisplayArea.GetFromWindowId(AppWindow.Id, DisplayAreaFallback.Primary);
     AppWindow.Move(new PointInt32(displayArea.WorkArea.X + (displayArea.WorkArea.Width - AppWindow.Size.Width) / 2,
       displayArea.WorkArea.Y + (displayArea.WorkArea.Height - AppWindow.Size.Height) / 2));
+    double scale = WinApi.GetScreenScaling(this);
+    AppWindow.Resize(new((int)Math.Round(500 * scale), (int)Math.Round(258 * scale)));
   }
 }
